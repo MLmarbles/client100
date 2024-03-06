61bd9bef4404b8145f0f89599b5883586026a85b
MLmarbles <118482430+MLmarbles@users.noreply.github.com>
2024-03-06 13:48:47 +0000

    finished

--- CS407Project/src/application/IncorrectActionException.java
+++ CS407Project/src/application/IncorrectActionException.java
@@ -1,5 +1,20 @@
 package application;
 
-public class IncorrectActionException {
+public class IncorrectActionException extends Exception {
+    private String message;
 
+    // Constructor with no parameters
+    public IncorrectActionException() {
+        this.message = "Incorrect action, please try again!";
+    }
+
+    // Constructor with a single parameter of type String
+    public IncorrectActionException(String message) {
+        this.message = message;
+    }
+
+    // Method to get the exception message
+    public String IncorrectActionMessage() {
+        return this.message;
+    }
 }
--- CS407Project/src/application/Main.java
+++ CS407Project/src/application/Main.java
@@ -1,6 +1,8 @@
 package application;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -9,6 +11,8 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -21,65 +25,201 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Main extends Application {
-	@FXML
-	public TextField enterModuleCode;
-	@FXML
-	public Button addModule;
+
 	@FXML
 	public TextArea serverText;
+
+	@FXML
+	public TextField mainTextField1;
+
+	@FXML
+	public Button stop1;
+
+	@FXML
+	public TextField enterModuleCode;
+
+	@FXML
+	public Button addModule;
+
+	@FXML
+	public Button removeModule;
 	
-    public static InetAddress host;
-    public static final int PORT = 1238;
+	@FXML
+	public TextField enterModuleCode1;
+	
+	@FXML
+	public Button addClass;
+	
+	@FXML
+	public Button removeClass;
+	
+	@FXML
+	public Button displaySchedule;
+	
+	@FXML
+	public Button enterTextAdd;
+	@FXML
+	public Button enterTextRemove;
+	@FXML
+	public Button stopServer;
+	
+	public String moduleCode;
 
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-		addModule.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent t) {
-				try {
-					host = InetAddress.getLocalHost();
-				} catch (UnknownHostException e) {
-					System.out.println("Host ID not found!");
-					System.exit(1);
-				}
-				Socket link = null;
-				try {
-					link = new Socket(host, PORT);
-					BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));
-					PrintWriter out = new PrintWriter(link.getOutputStream(), true);
+	public static InetAddress host;
+	public static final int PORT = 9523;
 
-			        out.println("ADD_MODULE " + enterModuleCode.getText());
-			        String response = "";
-					try {
-						response = in.readLine();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-			        serverText.appendText(response + "\n");
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		Scene scene = new Scene(root, 800, 600);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+    public void initialize() {
+        addModule.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+                try {
+					networkshit("ADD_MODULE", enterModuleCode.getText());
+				} catch (IncorrectActionException e) {
+					System.out.println(e.IncorrectActionMessage());
 					
 				}
-				 catch (IOException e) {
-					e.printStackTrace();
-				} finally {
-					try {
-						System.out.println("\n* Closing connection... *");
-						link.close(); // Step 4.
-					} catch (IOException e) {
-						System.out.println("Unable to disconnect/close!");
-						System.exit(1);
-					}
+            }
+        });
+
+        removeModule.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+                try {
+					networkshit("REMOVE_MODULE", enterModuleCode.getText());
+				} catch (IncorrectActionException e) {
+					System.out.println(e.IncorrectActionMessage());
+					
 				}
-			}
-		});
+            }
+        });
+        addClass.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                serverText.appendText("To add enter class details for " + enterModuleCode1.getText() + "\n"
+                					 +"Example (Monday, 9:00, 11:00, S206)" + "\n");
+            }
+        });
+        
+        enterTextAdd.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	moduleCode = enterModuleCode1.getText();
+            	String classData = mainTextField1.getText();
+            	try {
+					networkshit2("ADD_CLASS", moduleCode, classData);
+				} catch (IncorrectActionException e) {
+					System.out.println(e.IncorrectActionMessage());
+					
+				}
+            }
+        });
+        
+        removeClass.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                serverText.appendText("To remove enter class details for " + enterModuleCode1.getText() + "\n"
+                					 +"Example (Monday, 9:00, 11:00, S206" + "\n");
+            }
+        });
+        
+        enterTextRemove.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	moduleCode = enterModuleCode1.getText();
+            	String classData = mainTextField1.getText();
+            	try {
+					networkshit2("REMOVE_CLASS", moduleCode, classData);
+				} catch (IncorrectActionException e) {
+					System.out.println(e.IncorrectActionMessage());
+					
+				}
+            }
+        });
+        
+        displaySchedule.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	moduleCode = enterModuleCode1.getText();
+            	try {
+					networkshit("DISPLAY_SCHEDULE", moduleCode);
+				} catch (IncorrectActionException e) {
+					System.out.println(e.IncorrectActionMessage());
+					
+				}
+            }
+        });
+        
+        stopServer.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+					networkshit("STOP", "");
+				} catch (IncorrectActionException e) {
+					System.out.println(e.IncorrectActionMessage());
+					
+				} 
+            }
+        });
 
-            Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-            Scene scene = new Scene(root, 800, 600);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } 
-    
-
-    public static void main(String[] args) {
-        launch(args);
     }
+	
+    private void networkshit(String operation, String moduleCode) throws IncorrectActionException {
+        try {
+            host = InetAddress.getLocalHost();
+            Socket link = new Socket(host, PORT);
+            BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));
+            PrintWriter out = new PrintWriter(link.getOutputStream(), true);
+
+            out.println(operation + " " + moduleCode);
+            String response = in.readLine();
+            serverText.appendText(response + "\n");
+            if (response.startsWith("Error: ")) {
+                throw new IncorrectActionException(response);
+            }
+
+            System.out.println("\n* Closing connection... *");
+            link.close();
+        } catch (UnknownHostException e) {
+            System.out.println("Host ID not found!");
+            System.exit(1);
+        } catch (IOException e) {
+            
+        }
+    }
+    
+    private void networkshit2(String operation, String moduleCode, String classData) throws IncorrectActionException {
+        try {
+            host = InetAddress.getLocalHost();
+            Socket link = new Socket(host, PORT);
+            BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));
+            PrintWriter out = new PrintWriter(link.getOutputStream(), true);
+
+            out.println(operation + " " + moduleCode + " " + classData);
+            String response = in.readLine();
+            serverText.appendText(response + "\n");
+            if (response.startsWith("Error: ")) {
+                throw new IncorrectActionException(response);
+            }
+
+            System.out.println("\n* Closing connection... *");
+            link.close();
+        } catch (UnknownHostException e) {
+            System.out.println("Host ID not found!");
+            System.exit(1);
+        } catch (IOException e) {
+            
+        }
+    }
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
